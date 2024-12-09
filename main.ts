a397b4a638f5379bcc8cbe5ede5866ab73d4fd93
Okhotsk-hr <141467684+Okhotsk-hr@users.noreply.github.com>
2024-12-09 19:08:45 +0900

    変更してみた

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 input.onPinPressed(TouchPin.P0, function () {
-    basic.showIcon(IconNames.Butterfly)
+    basic.showIcon(IconNames.Angry)
 })
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Butterfly)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Meh)
+    basic.showIcon(IconNames.StickFigure)
 })
 basic.forever(function () {
 	
